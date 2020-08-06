ef5b59867b2a67770e3992aa22ea991df3cd610d
dimaggio <noesvenezuela@gmail.com>
2020-08-06 14:15:51 +0200

    agregamos el proyecto

---
+++ .history/resources/js/components/ExampleComponent_20200804144403.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                name:"", //Esta variable, mediante v-model esta relacionada con el input del formulario
+                description:"", //Esta variable, mediante v-model esta relacionada con el input del formulario
+                content:"", //Esta variable, mediante v-model esta relacionada con el input del formulario
+                update:0, /*Esta variable contrarolará cuando es una nueva tarea o una modificación, si es 0 significará que no hemos seleccionado
+                          ninguna tarea, pero si es diferente de 0 entonces tendrá el id de la tarea y no mostrará el boton guardar sino el modificar*/
+                arrayTasks:[], //Este array contendrá las tareas de nuestra bd
+            }
+        },
+        methods:{
+            getTasks(){
+                let me =this;
+                let url = '/tareas' //Ruta que hemos creado para que nos devuelva todas las tareas
+                axios.get(url).then(function (response) {
+                    //creamos un array y guardamos el contenido que nos devuelve el response
+                    me.arrayTasks = response.data;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+            },
+            saveTasks(){
+                let me =this;
+                let url = '/tareas/guardar' //Ruta que hemos creado para enviar una tarea y guardarla
+                axios.post(url,{ //estas variables son las que enviaremos para que crear la tarea
+                    'name':this.name,
+                    'description':this.description,
+                    'content':this.content,
+                }).then(function (response) {
+                    me.getTasks();//llamamos al metodo getTask(); para que refresque nuestro array y muestro los nuevos datos
+                    me.clearFields();//Limpiamos los campos e inicializamos la variable update a 0
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });   
+                
+            },
+            updateTasks(){/*Esta funcion, es igual que la anterior, solo que tambien envia la variable update que contiene el id de la
+                tarea que queremos modificar*/
+                let me = this;
+                axios.put('/tareas/actualizar',{
+                    'id':this.update,
+                    'name':this.name,
+                    'description':this.description,
+                    'content':this.content,
+                }).then(function (response) {
+                   me.getTasks();//llamamos al metodo getTask(); para que refresque nuestro array y muestro los nuevos datos
+                   me.clearFields();//Limpiamos los campos e inicializamos la variable update a 0
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            loadFieldsUpdate(data){ //Esta función rellena los campos y la variable update, con la tarea que queremos modificar
+                this.update = data.id
+                let me =this;
+                let url = '/tareas/buscar?id='+this.update;
+                axios.get(url).then(function (response) {
+                    me.name= response.data.name;
+                    me.description= response.data.description;
+                    me.content= response.data.content;
+                    
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                }); 
+            },
+            deleteTask(data){//Esta nos abrirá un alert de javascript y si aceptamos borrará la tarea que hemos elegido
+                let me =this;
+                let task_id = data.id
+                if (confirm('¿Seguro que deseas borrar esta tarea?')) {
+                    axios.delete('/tareas/borrar/'+task_id
+                    ).then(function (response) {
+                        me.getTasks();
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    }); 
+                }
+            },
+            clearFields(){/*Limpia los campos e inicializa la variable update a 0*/
+                this.name = "";
+                this.description = "";
+                this.content = "";
+                this.update = 0;
+            }
+        },
+        mounted() {
+           this.getTasks();
+        }
+    }
+</script>
---
+++ .history/resources/views/welcome.blade_20200805115749.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet"> 
+    </head>
+    <body>
+            <div id="app" class="content">
+                <div class="flex-center position-ref full-height">
+                    @if (Route::has('login'))
+                        <div class="top-right links">
+                            @auth
+                                <a href="{{ url('/home') }}">Home</a>
+                            @else
+                                <a href="{{ route('login') }}">Login</a>
+        
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
+                <example-component></example-component><!--Añadimos nuestro componente vuejs-->
+            </div>
+        <script src="{{asset('js/app.js')}}"></script> 
+    </body>
+</html>
