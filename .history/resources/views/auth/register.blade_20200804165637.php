5983dbb0cb6a0e21e4dbf28616527866afd1df8c
dimaggio <noesvenezuela@gmail.com>
2020-08-08 17:22:50 +0200

    roles

--- .history/resources/views/auth/register.blade_20200804165637.php
+++ .history/resources/views/auth/register.blade_20200804165637.php
@@ -61,8 +61,6 @@
                             </div>
                         </div>
 
-                        
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -70,8 +68,6 @@
                                 </button>
                             </div>
                         </div>
-
-
                     </form>
                 </div>
             </div>
